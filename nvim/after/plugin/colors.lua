90f90f34af7d3e4b2419259747d13b5f9aa469ef
kubil6y <lieqb2@gmail.com>
2024-04-30 16:09:24 +0300

    add tokyonight and change telescope viewer settings

--- nvim/after/plugin/colors.lua
+++ nvim/after/plugin/colors.lua
@@ -4,21 +4,57 @@ require("rose-pine").setup({
 	disable_float_background = true,
 	disable_italics = true,
 	highlight_groups = {
-        -- 2b2842,26233a
-        TreesitterContext = { bg = "#26233a" },
-        NvimTreeCursorLine = { bg = "#26233a" },
-        QuickFixLine = { bg = "#26233a", fg = "text" },
-        qfLineNr = { fg = "text" },
-        PmenuSel = { bg = "#26233a" },
+		-- 2b2842,26233a
+		TreesitterContext = { bg = "#26233a" },
+		NvimTreeCursorLine = { bg = "#26233a" },
+		QuickFixLine = { bg = "#26233a", fg = "text" },
+		qfLineNr = { fg = "text" },
+		PmenuSel = { bg = "#26233a" },
 
-        ["@comment.todo"] = { fg = "text", bg = "NONE", bold = true },
-        ["@comment.note"] = { fg = "text", bg = "NONE", bold = true },
-        ["@comment.error"] = { fg = "love", bg = "NONE", bold = true },
-        ["@comment.warning"] = { fg = "gold", bg = "NONE", bold = true },
+		["@comment.todo"] = { fg = "text", bg = "NONE", bold = true },
+		["@comment.note"] = { fg = "text", bg = "NONE", bold = true },
+		["@comment.error"] = { fg = "love", bg = "NONE", bold = true },
+		["@comment.warning"] = { fg = "gold", bg = "NONE", bold = true },
 
-        TelescopeSelection = { fg = "#e0def4", bg = "#26233a", bold = true },
-        TelescopeSelectionCaret = { fg = "#e0def4", bg = "#26233a" },
+		TelescopeSelection = { fg = "#e0def4", bg = "#26233a", bold = true },
+		TelescopeSelectionCaret = { fg = "#e0def4", bg = "#26233a" },
 	},
+})
+
+require("tokyonight").setup({
+	-- your configuration comes here
+	-- or leave it empty to use the default settings
+	style = "night", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
+	light_style = "day", -- The theme is used when the background is set to light
+	transparent = false, -- Enable this to disable setting the background color
+	terminal_colors = true, -- Configure the colors used when opening a `:terminal` in [Neovim](https://github.com/neovim/neovim)
+	styles = {
+		-- Style to be applied to different syntax groups
+		-- Value is any valid attr-list value for `:help nvim_set_hl`
+		comments = { italic = true },
+		keywords = { italic = true },
+		functions = {},
+		variables = {},
+		-- Background styles. Can be "dark", "transparent" or "normal"
+		sidebars = "dark", -- style for sidebars, see below
+		floats = "dark", -- style for floating windows
+	},
+	sidebars = { "qf", "help" }, -- Set a darker background on sidebar-like windows. For example: `["qf", "vista_kind", "terminal", "packer"]`
+	day_brightness = 0.3, -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
+	hide_inactive_statusline = false, -- Enabling this option, will hide inactive statuslines and replace them with a thin border instead. Should work with the standard **StatusLine** and **LuaLine**.
+	dim_inactive = false, -- dims inactive windows
+	lualine_bold = false, -- When `true`, section headers in the lualine theme will be bold
+
+	--- You can override specific color groups to use other groups or a hex color
+	--- function will be called with a ColorScheme table
+	---@param colors ColorScheme
+	on_colors = function(colors) end,
+
+	--- You can override specific highlights to use other groups or a hex color
+	--- function will be called with a Highlights and ColorScheme table
+	---@param highlights Highlights
+	---@param colors ColorScheme
+	on_highlights = function(highlights, colors) end,
 })
 
 require("gruvbox").setup({
@@ -62,10 +98,11 @@ function _G.set_colors(color)
 	vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
 	vim.api.nvim_set_hl(0, "hl_inlay_hints", { fg = "#59566d" })
 
-    if color == "gruvbuddy"  then
-        vim.cmd([[ hi LineNr guifg=#383b40 ]])
-        vim.cmd([[ hi TelescopeSelection guibg=#383b40 ]])
-    end
+	if color == "gruvbuddy" then
+		vim.cmd([[ hi LineNr guifg=#383b40 ]])
+		vim.cmd([[ hi TelescopeSelection guibg=#333951 ]])
+		vim.cmd([[hi @function.method.call gui=bold guifg=#f8fe7a ]])
+	end
 end
 
-set_colors()
+set_colors("tokyonight-night")
--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -13,6 +13,20 @@ require("telescope").setup({
 		path_display = { "smart" },
 		file_ignore_patterns = {},
 
+        layout_config = {
+            horizontal = {
+                prompt_position = "bottom",
+                preview_width = 0.55,
+                results_width = 0.8,
+            },
+            vertical = {
+                mirror = false,
+            },
+            width = 0.87,
+            height = 0.80,
+            preview_cutoff = 120,
+        },
+
 		mappings = {
 			i = {
 				["<C-u>"] = false, -- clears input
